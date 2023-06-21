6ee5882337506bb355d42b44efff120a29b7d2dc
Wang Song <NJUSTer_ws@163.com>
2023-06-21 19:28:06 +0800

    修改MainScreen.kt | 2023.6.21 | ws

--- app/src/main/java/com/example/news/ui/screens/MainScreen.kt
+++ app/src/main/java/com/example/news/ui/screens/MainScreen.kt
@@ -2,15 +2,28 @@ package com.example.news.ui.screens
 
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.news.R
 import com.example.news.ui.theme.NewsTheme
 
@@ -19,36 +32,95 @@ import com.example.news.ui.theme.NewsTheme
 
 /*TODO : 更改按钮，文字样式*/
 @Composable
-fun MainScreen(
+fun MainScreen(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Background_image(img_name = R.drawable.main_one)
+    }
+}
 
-) {
-    Background_image(img_name = R.drawable.main)
-    Column {
-        Text(text = stringResource(R.string.main_title))
-        Text(text = stringResource(R.string.main_description))
-        Row() {
-            Button(onClick = { /*TODO*/ }) {
-                Text(text = "注册")
-            }
-            Button(onClick = { /*TODO*/ }) {
-                Text(text = "登录")
-            }
+/**
+ * 首页文本区
+ */
+@Composable
+fun MainScreenText(modifier: Modifier = Modifier) {
+    Column(
+        // 设置列布局的高度和宽度设为可用的最大值
+        modifier = modifier
+            .fillMaxSize()
+            .padding(start = 18.dp),
+        // 文本元素底部对齐
+        verticalArrangement = Arrangement.Bottom,
+        // 文本元素水平居中
+        horizontalAlignment = Alignment.Start
+    ) {
+        Text(
+            text = stringResource(R.string.main_title),
+            fontSize = 30.sp,
+            color = Color.Black,
+            modifier = Modifier
+                .padding(bottom = 14.dp)
+        )
+        Text(
+            text = stringResource(R.string.main_description),
+            color = Color.Black,
+            modifier = Modifier
+                .padding(bottom = 20.dp)
+        )
+        MainScreenButton()
+    }
+}
+
+/**
+ * 首页的按钮
+ */
+@Composable
+fun MainScreenButton(modifier: Modifier = Modifier) {
+    Row(
+        //
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 50.dp),
+        // 水平居中
+        horizontalArrangement = Arrangement.Center,
+        // 垂直居中
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier.padding(end = 20.dp)    // 调整两个按钮之间的距离
+        ) {
+            Text(text = "注册")
+        }
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "登录")
         }
     }
 }
 
 /* TODO: 更改背景图片 */
+/**
+ * 背景图片处理
+ */
 @Composable
-fun Background_image(
-    @DrawableRes img_name: Int,
-    modifier: Modifier = Modifier
-) {
-    Image(painter = painterResource(id = img_name), contentDescription = "background_img")
+fun Background_image(@DrawableRes img_name: Int, modifier: Modifier = Modifier) {
+    // 外层使用 Box 布局包裹
+    Box {
+        Image(
+            painterResource(id = img_name),
+            contentDescription = "background_img",
+            // 缩放背景
+            contentScale = ContentScale.Crop
+        )
+        MainScreenText()
+    }
 }
 
 @Preview
 @Composable
-fun prev() {
+fun Prev() {
     NewsTheme {
         MainScreen()
     }
