349d5cb8367811fdf285e74ec0dfc29a6c040476
Wang Song <NJUSTer_ws@163.com>
2023-06-23 10:10:10 +0800

    登录、注册页面调整 | 2023.6.23 | ws

--- app/src/main/java/com/example/news/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/example/news/ui/screens/LoginScreen.kt
@@ -1,7 +1,6 @@
 package com.example.news.ui.screens
 
 import android.util.Log
-import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -16,15 +15,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.AbsoluteRoundedCornerShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -32,12 +26,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -184,49 +176,6 @@ fun LoginScreen(
         }
     }
 }
-
-/*
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun InputField(
-    */
-/* 状态提升 *//*
-
-    value: String,                  // 当前要显示的值
-    onValueChange: (String) -> Unit,// 值更改时触发，以便可以在其他位置更新状态
-    modifier: Modifier = Modifier
-) {
-    TextField(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(start = 15.dp, end = 15.dp)
-            .clip(RoundedCornerShape(20)),
-        singleLine = true,          // 水平滑动
-        value = value,              // 框中的值
-        onValueChange = onValueChange,
-    )
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun PasswordField(
-    value: String,                  // 当前要显示的值
-    onValueChange: (String) -> Unit,// 值更改时触发，以便可以在其他位置更新状态
-    modifier: Modifier = Modifier
-) {
-    TextField(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(start = 15.dp, end = 15.dp)
-            .clip(RoundedCornerShape(20)),
-        singleLine = true,          // 水平滑动
-        value = value,              // 框中的值
-        onValueChange = onValueChange,
-        visualTransformation = PasswordVisualTransformation()
-    )
-
-}
-*/
 
 @Preview
 @Composable
--- app/src/main/java/com/example/news/ui/screens/Utils.kt
+++ app/src/main/java/com/example/news/ui/screens/Utils.kt
@@ -3,36 +3,41 @@ package com.example.news.ui.screens
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.TextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+
+
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun InputField(
     /* 状态提升 */
     value: String,                  // 当前要显示的值
     onValueChange: (String) -> Unit,// 值更改时触发，以便可以在其他位置更新状态
     modifier: Modifier = Modifier
-
 ) {
     TextField(
         modifier = Modifier
             .fillMaxWidth()
             .padding(start = 15.dp, end = 15.dp)
             .clip(RoundedCornerShape(20)),
-        singleLine = true,          // 水平滑动
+        singleLine = true,          // 单行
+        maxLines = 1,               // 最多一行
         value = value,              // 框中的值
-        onValueChange = onValueChange,
-
+        onValueChange = onValueChange,  // 触发事件
+        colors = TextFieldDefaults.textFieldColors(
+            backgroundColor = Color.White
         )
+    )
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PasswordField(
     value: String,                  // 当前要显示的值
@@ -44,9 +49,14 @@ fun PasswordField(
             .fillMaxWidth()
             .padding(start = 15.dp, end = 15.dp)
             .clip(RoundedCornerShape(20)),
-        singleLine = true,          // 水平滑动
+        singleLine = true,          // 单行
+        maxLines = 1,               // 最多一行
         value = value,              // 框中的值
-        onValueChange = onValueChange,
-        visualTransformation = PasswordVisualTransformation()
+        onValueChange = onValueChange,colors = TextFieldDefaults.textFieldColors(
+            backgroundColor = Color.White
+        ),
+        visualTransformation = PasswordVisualTransformation(),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
     )
+
 }
